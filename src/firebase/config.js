09cd3eb03a79b7c7c50db8c5ec44267c058a3560
Sriram <sriram.tcs@gmail.com>
2025-11-03 09:49:10 +0530

    updated env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC4jlyirLwczS-7UBF32kh08Hgi-lo7eNw",
-  authDomain: "terra-d9ff9.firebaseapp.com",
-  projectId: "terra-d9ff9",
-  storageBucket: "terra-d9ff9.firebasestorage.app",
-  messagingSenderId: "183787893626",
-  appId: "1:183787893626:web:944ac9af146cf9f396dcc3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
